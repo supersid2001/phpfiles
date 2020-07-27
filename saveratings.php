8e7cbb0461f8dcf4ef8a439990c33c146c9a3380
Siddharth Ananth <supersid2001@gmail.com>
2020-07-27 18:38:07 +0530

    Changes made

--- saveratings.php
+++ saveratings.php
@@ -9,24 +9,18 @@
 	$username = $_POST["username"];
 	$string = $_POST["ratings"];
 	$json = array(
-		'num_ratings' => 0,
-		'total_rating' => 0,
-		'avg_rating' => 0,
+		'username' => null,
+		'rating' => 0,
 		'likes' => "", 
 		'dislikes' => "",
 		'dislikes_exp' => "",
 		'bugs' => "",
 		'comments' => "",
-		'recommended' => 0);
+		'recommended' => 0
+		'feedback_type' => 0,
+    	'meeting_id' => 0,
+    	'version' => "2.9.1");
 	$json = json_decode($string, true) or die(mysqli_error($con));
-	$namecheckquery = "SELECT username FROM ratings WHERE username = '" .  $username . "'; ";
-	$namecheck = mysqli_query($con, $namecheckquery) or die("2: Name check query failed");
-	if(mysqli_num_rows($namecheck != 1))
-	{
-		echo "5: Either no user with name or more than one user with name";
-		exit();
-	}
-	$updatequery = "UPDATE ratings SET num_ratings = " . $json['num_ratings'] ." , total_rating = " . $json['total_rating'] ." , avg_rating = " . $json['avg_rating'] ." , likes = '" . $json['likes'] ."', dislikes = '" . $json['dislikes'] ."', dislikes_exp = '" . $json['dislikes_exp'] ."', bugs = '" . $json['bugs'] ."', comments = '" . $json['comments'] ."', Recommend = " . $json['recommended'] ." WHERE username = '" . $username ."';";
-	mysqli_query($con, $updatequery) or die(mysqli_error($con));
+	mysqli_query($con, "INSERT INTO ratings ( `username`, `rating`, `likes`, `dislikes`, `dislikes_exp`, `bugs`, `comments`, `Recommend`, `feedback_type`, `meeting_id`, `version`) VALUES ('". $json['username']"', " . $json['rating'] .", '" .$json['likes'] ."', '" . $json['dislikes'] ."', '". $json['dislikes_exp']"', '". $json['bugs'] ."', '". $json['comments']"', '". $json['recommended'] ."', ". $json['feedback_type'] .", " . $json['meeting_id'] .", " . $json['version'] .");") or die(mysqli_error($con));
 	echo "0";
 ?>
